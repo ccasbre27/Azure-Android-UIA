18a541c6c73eda9cc0784a62ca33ae82015ff7ce
ccasbre27@hotmail.com <ccasbre27@hotmail.com>
2016-04-03 19:25:33 -0600

    Init para demo1

--- app/src/main/java/com/example/itadmin/demouia/MainActivity.java
+++ app/src/main/java/com/example/itadmin/demouia/MainActivity.java
@@ -19,7 +19,7 @@ import com.microsoft.windowsazure.mobileservices.table.TableOperationCallback;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private MobileServiceClient mClient;
     EditText edtNombre;
@@ -29,111 +29,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // referencia al EditText
-        edtNombre = (EditText) findViewById(R.id.edtUserName);
 
-        // se establece el onClickListener el botón
-        findViewById(R.id.btnAgregar).setOnClickListener(this);
-
-        try {
-            mClient = new MobileServiceClient(
-                    "https://pre-kia.azurewebsites.net",
-                    this
-            );
-        }
-        catch (Exception e)
-        {
-
-        }
-
-        
-        /*
-        List<Pair<String, String>> parameters = new ArrayList<Pair<String, String>>();
-
-        ListenableFuture<JsonElement> result =  mClient.invokeApi("alumnos","GET",parameters);
-
-        Futures.addCallback(result, new FutureCallback<JsonElement>() {
-            @Override
-            public void onFailure(Throwable exc) {
-
-                Toast.makeText(MainActivity.this,"Error al agregar",Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onSuccess(JsonElement result) {
-
-                Toast.makeText(MainActivity.this,"Estudiante agregado",Toast.LENGTH_SHORT).show();
-            }
-
-
-        });
-        */
 
     }
 
-    @Override
-    public void onClick(View v) {
-
-        if (v.getId() == R.id.btnAgregar)
-        {
-            agregarUsuario();
-        }
-
-    }
 
     private void agregarUsuario()
     {
-        // se obtiene el texto ingresado
-        String nombreUsuario = edtNombre.getText().toString();
 
-        // se verifica si está vacío
-        if(nombreUsuario.isEmpty())
-        {
-            // si es así se indica que se debe ingresar datos
-            Toast.makeText(getApplicationContext(), "Debe ingresar datos", Toast.LENGTH_SHORT).show();
-        }
-        else
-        {
-            // en caso contrario se procede a insertar el nuevo elemento
-            Estudiante item = new Estudiante();
-            item.name = nombreUsuario; // se establece el nombre del usuario con el valor que el usuario ingresó
-
-            // se obtiene referencia a la tabla Alumno
-            MobileServiceTable<Estudiante> table = mClient.getTable("Alumno",Estudiante.class);
-
-            // se inserta en la tabla Alumno
-            table.insert(item, new TableOperationCallback<Estudiante>() {
-                public void onCompleted(Estudiante entity, Exception exception, ServiceFilterResponse response) {
-                    if (exception == null) {
-                        Toast.makeText(MainActivity.this, "Estudiante agregado", Toast.LENGTH_SHORT).show();
-                        edtNombre.setText("");
-                    } else {
-                        Toast.makeText(MainActivity.this, "Error al agregar", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-
-            /*
-            ListenableFuture<Estudiante> result =  table.insert(item);
-
-            Futures.addCallback(result, new FutureCallback<Estudiante>() {
-                @Override
-                public void onFailure(Throwable exc) {
-
-                    Toast.makeText(MainActivity.this,"Error al agregar",Toast.LENGTH_SHORT).show();
-                }
-
-                @Override
-                public void onSuccess(Estudiante result) {
-
-                    Toast.makeText(MainActivity.this,"Estudiante agregado",Toast.LENGTH_SHORT).show();
-                }
-
-
-            });
-            */
-
-        }
 
     }
 }
